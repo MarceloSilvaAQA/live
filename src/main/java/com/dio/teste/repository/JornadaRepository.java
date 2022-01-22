1d2eb4a672046f824623ffebc5302ff742648180
MarceloSilvaAQA <marcelohsilva36@gmail.com>
2022-01-21 21:53:30 -0300

    .

---
+++ src/main/java/com/dio/teste/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package com.dio.teste.repository;
+
+import com.dio.teste.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
